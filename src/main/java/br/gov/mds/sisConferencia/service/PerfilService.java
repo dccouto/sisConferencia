622c3dea6bc58f1a794f5981af9f73a078926df2
bruno.silveira <bruno.silveira@spcm.com.br>
2023-07-21 15:19:01 -0300

    GenericService Merge Resolve

--- src/main/java/br/gov/mds/sisConferencia/service/PerfilService.java
+++ src/main/java/br/gov/mds/sisConferencia/service/PerfilService.java
@@ -1,31 +1,15 @@
 package br.gov.mds.sisConferencia.service;
 
-import br.gov.mds.sisConferencia.config.mapper.EntityMapper;
-import br.gov.mds.sisConferencia.service.dto.PerfilDTO;
-import br.gov.mds.sisConferencia.service.mapper.PerfilMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import br.gov.mds.sisConferencia.config.mapper.EntityMapper;
 import br.gov.mds.sisConferencia.models.Perfil;
 import br.gov.mds.sisConferencia.repository.PerfilRepository;
 import br.gov.mds.sisConferencia.service.dto.PerfilDTO;
-
-import javax.transaction.Transactional;
+import br.gov.mds.sisConferencia.service.mapper.PerfilMapper;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
-public class PerfilService extends GenericService<Perfil, Long, PerfilDTO> {
-
-	public PerfilService(PerfilRepository repository, EntityMapper<PerfilDTO, Perfil> mapper) {
-		super(repository, mapper);
-	}
-
-	@Transactional
-	public PerfilDTO salvar(PerfilDTO perfilDTO) {
-		return this.mapper.toDto(save(this.mapper.toEntity(perfilDTO)));
-	}
+public class PerfilService extends GenericService<Perfil , Long, PerfilDTO> {
 
 	public PerfilService(PerfilRepository repository, PerfilMapper mapper) {
 		super(repository, mapper);
@@ -33,11 +17,10 @@ public class PerfilService extends GenericService<Perfil, Long, PerfilDTO> {
 
 	@Transactional
 	public Perfil atualizar(Long id, Perfil perfil) {
-
 		Perfil existingPerfil = findById(id);
 		existingPerfil.setDescricao(perfil.getDescricao());
 		return save(existingPerfil);
-
+		
 	}
 
 }
