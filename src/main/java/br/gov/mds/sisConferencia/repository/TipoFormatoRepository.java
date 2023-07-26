1b7a95f3a5f023e355e2629a5007365079f1ef19
bruno.silveira <bruno.silveira@spcm.com.br>
2023-07-26 08:41:31 -0300

    anotações

--- src/main/java/br/gov/mds/sisConferencia/repository/TipoFormatoRepository.java
+++ src/main/java/br/gov/mds/sisConferencia/repository/TipoFormatoRepository.java
@@ -3,7 +3,9 @@ package br.gov.mds.sisConferencia.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.gov.mds.sisConferencia.models.TipoFormato;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TipoFormatoRepository extends JpaRepository<TipoFormato, Long> {
 
 }
